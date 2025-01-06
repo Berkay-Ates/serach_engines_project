870c74b91e271ad135212762808862605d3d6429
Berkay Ates <atesberkay2356@gmail.com>
2025-01-06 11:03:32 +0300

    refactor: comment out mds_dimension_reduction method in DimensionReducerMethods

--- modules/dimension_reducer/dimension_reducer_methods.py
+++ modules/dimension_reducer/dimension_reducer_methods.py
@@ -11,9 +11,9 @@ class DimensionReducerMethods:
         pca = PCA(n_components=2)
         return pca.fit_transform(data)
 
-    def mds_dimension_reduction(self, data: list) -> np.ndarray:
-        mds = MDS(n_components=2)
-        return mds.fit_transform(data)
+    # def mds_dimension_reduction(self, data: list) -> np.ndarray:
+    #     mds = MDS(n_components=2)
+    #     return mds.fit_transform(data)
 
     def tsne_dimension_reduction(self, data: np.array) -> np.ndarray:
         tsne_reduction = TSNE(n_components=2)
